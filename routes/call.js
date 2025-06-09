4efe9d059fc751a13c8c956af7254a71f05b00f9
Tushar <tusharupworkdev@gmail.com>
2025-06-09 20:55:57 +0530

    fixes

--- routes/call.js
+++ routes/call.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { CallController } from "../controller/call";
+import { CallController } from "../controller/call.js";
 
 const callRouter = express.Router();
 const callController = new CallController();
 
-callRouter.post("/makeCall", callController.CreateCall());
+callRouter.post("/makeCall", callController.CreateCall);
 
 export default callRouter;
